77ed16ebdc454e61a8b627aa897906c63ec30f24
sidhant729 <sidhant1841.be21@chitkara.edu.in>
2024-01-12 20:33:03 +0530

    Added The Solution Of Longest Substring With Atmost 2 Distinct Charcters Problem

---
+++ Test_2/Arrays_And_Strings/Longest_Substring_With_Atmost_2_Distinct_Characters/Longest_Substring_With_Atmost_2_Distinct_Characters.java
@@ -0,0 +1,34 @@
+// Problem Link -> https://www.codingninjas.com/studio/problems/longest-substring-with-at-most-two-distinct-characters_3125863?leftPanelTabValue=PROBLEM
+
+import java.util.HashMap;
+
+public class Solution {
+	public static int lengthOfLongestSubstring(String s) {
+		// Write your code here.
+		HashMap<Character,Integer> mp = new HashMap<>();
+		int low = 0;
+		int high = 0;
+		int res = 0;
+		while(high < s.length()) {
+			char currChar = s.charAt(high);
+			mp.put(currChar, mp.getOrDefault(currChar, 0) + 1);
+			if(mp.size() > 2) {
+				while(mp.size() > 2) {
+					int currOccurences = mp.get(s.charAt(low));
+					currOccurences--;
+					if(currOccurences == 0) {
+						mp.remove(s.charAt(low));
+					} else {
+						mp.put(s.charAt(low), currOccurences);
+					}
+					low++;
+				}
+				res = Math.max(res, high - low + 1);
+			} else {
+				res = Math.max(res, high - low + 1);
+			}
+			high++;
+		}
+		return res;
+	}
+}
