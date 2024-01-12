45f392449704ef0508da9e3550106a526c96072c
sidhant729 <sidhant1841.be21@chitkara.edu.in>
2024-01-12 20:30:46 +0530

    Added The Solution Of Merge K Sorted Lists Problem

--- Test_2/Arrays_And_Strings/Merge_K_Sorted_Lists/Efficient_1.java
+++ Test_2/Arrays_And_Strings/Merge_K_Sorted_Lists/Efficient_1.java
@@ -1,5 +1,15 @@
 // Problem Link -> https://leetcode.com/problems/merge-k-sorted-lists/description/
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     private static ListNode mergeSort(ListNode a, ListNode b) {
         if(a == null) return b;
